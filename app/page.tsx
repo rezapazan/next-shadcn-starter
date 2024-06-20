b11a9d432460ea2b4de449696d080e3ade2be3be
Reza Pazan <rez.paz78@gmail.com>
2024-06-20 21:13:59 +0330

    Init Commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,23 @@
 import Link from 'next/link'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle
+} from '@/components/ui/card'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue
+} from '@/components/ui/select'
+import { ThemeToggle } from '@/components/theme-toggle'
 
 export default function Home() {
   return (
@@ -6,12 +25,44 @@ export default function Home() {
       <div className='container'>
         <h1 className='text-3xl font-bold'>Next TS Starter </h1>
 
-        <div className='flex items-center'>
-          <Link className='mr-2' href={'/about'}>
-            About
-          </Link>
-          <Link href={'/team'}>Team</Link>
-        </div>
+        <ThemeToggle />
+
+        <Card className='mt-12 w-[350px]'>
+          <CardHeader>
+            <CardTitle>Create project</CardTitle>
+            <CardDescription>
+              Deploy your new project in one-click.
+            </CardDescription>
+          </CardHeader>
+          <CardContent>
+            <form>
+              <div className='grid w-full items-center gap-4'>
+                <div className='flex flex-col space-y-1.5'>
+                  <Label htmlFor='name'>Name</Label>
+                  <Input id='name' placeholder='Name of your project' />
+                </div>
+                <div className='flex flex-col space-y-1.5'>
+                  <Label htmlFor='framework'>Framework</Label>
+                  <Select>
+                    <SelectTrigger id='framework'>
+                      <SelectValue placeholder='Select' />
+                    </SelectTrigger>
+                    <SelectContent position='popper'>
+                      <SelectItem value='next'>Next.js</SelectItem>
+                      <SelectItem value='sveltekit'>SvelteKit</SelectItem>
+                      <SelectItem value='astro'>Astro</SelectItem>
+                      <SelectItem value='nuxt'>Nuxt.js</SelectItem>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+            </form>
+          </CardContent>
+          <CardFooter className='flex justify-between'>
+            <Button variant='outline'>Cancel</Button>
+            <Button>Deploy</Button>
+          </CardFooter>
+        </Card>
       </div>
     </section>
   )
